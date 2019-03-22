877ef5e4082df6b6890f854474e5adb48dbe568b
Andres Fernandez <lafvg12@hotmail.com>
2019-03-21 21:27:37 -0500

    my first class

--- dogs_spec.rb
+++ dogs_spec.rb
@@ -2,13 +2,14 @@ require_relative 'dogs'
 
 RSpec.describe Dogs do
     it "responds to property :name" do
-        logan = Dogs.new('Logan el grande')
+        logan = Dogs.new('Logan el grande') # faltaba el initialize
         expect(logan).to respond_to(:name)
     end
 
     # breed ::: RAZA
     it "responds to property :breed" do
-
+        logan = Dogs.new("Logan el grande")
+        expect(logan).to respond_to(:breed)
     end
 
     it "responds to property :age" do
